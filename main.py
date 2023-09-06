b4ee567d8e56d54e7b0a02eb52970cdb7cb352eb
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-09-06 17:58:19 -0600

    comentario: guia para normalizar datos

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ visu.save_histograms(data)
 visu.save_scatter_plots(data, correlation_data)
 
 visu.save_histogram_correlations(data)
+
+# Z = 1/sqrt(n) * ((x-mu)/sigma)
+# data.mean()
+# x = data["AGE"]
+# x = data.loc[:, "AGE"]
+# x.mean()
