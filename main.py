826b08cb7d0e8f6974eba571796e378768b2619f
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-09-13 19:24:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ correlation_data = proc.get_correlations(data)
 
 normalized_data = proc.normalize_diabetes_data(data)
 
-input_data = normalized_data["BMI"]
-output_data = normalized_data["Y"]
-
 training, test = proc.split_data(normalized_data, 0.7)
 training_input = training["BMI"]
 training_output = training["Y"]
-test_input = training["BMI"]
-test_output = training["Y"]
+test_input = test["BMI"]
+test_output = test["Y"]
 
-model = proc.simple_linear_regression(training_input, training_output)
+model = proc.simple_linear_regression(training_input,
+                                      training_output)
 test_predictions = proc.test_predictions(model, test_input)
 
 coefficients = proc.get_coefficients(model)
 print("Coefficients: ", coefficients)
-MSE = proc.get_mean_squared_error(test_output, test_predictions)
+MSE = proc.get_mean_squared_error(test_output,
+                                  test_predictions)
 print("Mean Squared Error: ", MSE)
-R2 = proc.get_coefficient_determination(test_output, test_predictions)
+R2 = proc.get_coefficient_determination(test_output,
+                                        test_predictions)
 print("R² Score: ", R2)
 # var(mean) - var(line) / var(mean)
 # R² es  el porcentaje de variación descrito por la relación de dos variables
