54026b3b13150f1e739bc9957fddbd33e112a516
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-03 19:58:09 -0600

    Update decision_trees.py

--- decision_trees.py
+++ decision_trees.py
@@ -4,16 +4,20 @@ from sklearn.tree import export_text
 import utils.visuals as visu
 import pandas as pd
 import utils.processing as proc
+import numpy as np
 
 iris = load_iris()
-iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = np.expand_dims(iris.target, axis=1)
+irisArray = np.concatenate((iris.data, y), axis=1)
+iris.feature_names.append("target")
+iris_df = pd.DataFrame(irisArray, columns=iris.feature_names)
 visu.save_scatter_plots(iris_df, iris_df.corr())
 
-X, y = iris.data, iris.target
-# proc.split_data()
+train_data, test_data = proc.split_data(iris_df, 0.7)
+# train_data split --> x_train_data, y_train_data
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, y)
-y_predict = clf.predict(X)
+y_predict = clf.predict(X) # x_test_data
 r = export_text(clf, feature_names=iris['feature_names'])
 print(r)
 proc.get_confusion_matrix(iris.target, y_predict, iris.target_names)
