d4fc978e0daa26b8534b1b2bcd52607b544def8d
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-04 19:21:10 -0600

    add pptx and final commit for decision trees

--- decision_trees.py
+++ decision_trees.py
@@ -14,11 +14,15 @@ iris_df = pd.DataFrame(irisArray, columns=iris.feature_names)
 visu.save_scatter_plots(iris_df, iris_df.corr())
 
 train_data, test_data = proc.split_data(iris_df, 0.7)
-# train_data split --> x_train_data, y_train_data
+x_train_data = train_data[train_data.columns[0:4]]
+y_train_data = train_data["target"]
+x_test_data = test_data[test_data.columns[0:4]]
+y_test_data = test_data["target"]
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
-y_predict = clf.predict(X) # x_test_data
+clf = clf.fit(x_train_data, y_train_data)
+y_predict = clf.predict(x_test_data)  # x_test_data
+iris.feature_names.remove("target")
 r = export_text(clf, feature_names=iris['feature_names'])
 print(r)
-proc.get_confusion_matrix(iris.target, y_predict, iris.target_names)
-
+confusion_matrix = proc.get_confusion_matrix(y_test_data, y_predict, iris.target_names)
+visu.save_confusion_matrix(confusion_matrix)
