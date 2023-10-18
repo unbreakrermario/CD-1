e3387f506d63215e13d85e26e335ad6930324744
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-17 19:48:57 -0600

    Update airports_data.py

--- airports_data.py
+++ airports_data.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import utils.imports as inp
 import utils.visuals as visu
 import plotly.graph_objects as go
@@ -6,6 +8,9 @@ flights = inp.read_flights_dataset("data/flightsJan.csv")
 airports = inp.read_flights_dataset("data/airports.csv")
 visu.plot_airports_geoscatter(airports)
 print(flights.head())
+
+flights_count_origin = flights["ORIGIN"].value_counts()
+flights_count_dest = flights["DEST"].value_counts()
 visu.save_histogram(flights, "DAY_OF_MONTH")
 visu.save_histogram(flights, "TAIL_NUM")
 visu.save_histogram(flights, "OP_CARRIER")
