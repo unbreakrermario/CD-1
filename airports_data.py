5f6a5bb633b957672f922e26cdb241e4e9d38a61
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-24 19:32:08 -0600

    Update airports_data.py

--- airports_data.py
+++ airports_data.py
@@ -37,7 +37,22 @@ idx_filtered = flights_top[flights_top["CANCELLED"] == 0].index
 flights_top.drop(idx_filtered, inplace=True)
 
 # Use GeoScatter to show the airports with more cancelled flights
+flights_count_origin = flights_top["ORIGIN"].value_counts()
+flights_count_dest = flights_top["DEST"].value_counts()
 
+F = flights_count_origin.to_frame()
+F = F.rename(columns={'count': 'count_origin'})
+new_airports = airports.set_index('AIRPORT').join(F)
+new_airports = new_airports.reset_index()
+
+F = flights_count_dest.to_frame()
+F = F.rename(columns={'count': 'count_destination'})
+new_airports = new_airports.set_index('AIRPORT').join(F)
+new_airports = new_airports.reset_index()
+
+new_airports["count_total"] = new_airports["count_origin"] + new_airports["count_destination"]
+new_airports = new_airports.sort_values(by='count_total', ascending=False)
+visu.plot_airports_geoscatter(new_airports.head(n=15))
 
 visu.save_histogram(flights_top, "DAY_OF_MONTH")
 visu.save_histogram(flights_top, "TAIL_NUM")
